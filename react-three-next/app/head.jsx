3d99b7c43384ec90bf1a87ef132b74f76e4fd701
54rtor1 <1929sartori@gmail.com>
2025-04-23 09:24:01 -0300

    defines meta tags

--- react-three-next/app/head.jsx
+++ react-three-next/app/head.jsx
@@ -1,19 +1,20 @@
-const title = 'React Three Next Starter'
-const url = 'https://react-three-next.vercel.app/'
-const description = 'The easiest and fastest way to create a 3D website using React Three Fiber and NextJS'
-const author = 'Author'
-const twitter = '@pmndrs'
+const title = 'Litoral Obsoleto'
+const url = 'https://litoral-obsoleto.vercel.app/'
+const description = 'Sea-level rise and coastal memory decay.'
+const author = 'Julia '
 
 export default function Head() {
   return (
     <>
-      {/* Recommended Meta Tags */}
+
       <meta charSet='utf-8' />
       <meta name='language' content='english' />
       <meta httpEquiv='content-type' content='text/html' />
       <meta name='author' content={author} />
-      <meta name='designer' content={author} />
-      <meta name='publisher' content={author} />
+      <meta name='title' content={title} />
+      <meta name='url' content={url} />
+      <meta name='description' content={description} />
+      <meta name='keywords' content='litoral, obsoleto, sea-level rise, coastal memory decay' />
     </>
   )
 }
