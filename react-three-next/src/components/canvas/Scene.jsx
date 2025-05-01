06261c286d46b39d1390487608342bd55170e4d8
54rtor1 <1929sartori@gmail.com>
2025-05-01 18:52:03 -0300

    adds toggable photo background

--- react-three-next/src/components/canvas/Scene.jsx
+++ react-three-next/src/components/canvas/Scene.jsx
@@ -3,14 +3,22 @@ import { Preload } from '@react-three/drei'
 import * as THREE from 'three'
 import CoastalParticlesWrapper from '@/components/canvas/CoastalParticlesWrapper'
 
-
-export default function Scene({ scrollY, ...props }) {
+export default function Scene({ scrollY, useImageBackground = false, ...props }) {
   return (
-    <Canvas {...props}
+    <Canvas
+      {...props}
       onCreated={(state) => {
-        state.gl.toneMapping = THREE.AgXToneMapping
+        state.gl.toneMapping = THREE.ACESFilmicToneMapping
         state.gl.physicallyCorrectLights = true
-        state.scene.background = new THREE.Color(0x000000) // <<< important
+
+        if (useImageBackground) {
+          const textureLoader = new THREE.TextureLoader()
+          textureLoader.load('videos/background2.png', (texture) => {
+            state.scene.background = texture
+          })
+        } else {
+          state.scene.background = new THREE.Color(0x112233)
+        }
       }}
     >
       <CoastalParticlesWrapper />
