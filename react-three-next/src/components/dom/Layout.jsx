b8075ef019d93717302b1f254702445fa90cbff7
54rtor1 <1929sartori@gmail.com>
2025-05-01 18:52:03 -0300

    adds toggable photo background

--- react-three-next/src/components/dom/Layout.jsx
+++ react-three-next/src/components/dom/Layout.jsx
@@ -19,6 +19,7 @@ const Layout = ({ children }) => {
       </Scroll>
       <Scene
         scrollY={scrollY}
+        useImageBackground={true}
         style={{
           position: 'fixed',
           top: 0,
