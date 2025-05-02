609ddde249db6ccc50cc83608b9536542d7ef444
54rtor1 <1929sartori@gmail.com>
2025-05-02 11:39:37 -0300

    fixes hook to fetch data dynamically

--- react-three-next/src/templates/hooks/useSeaLevel.jsx
+++ react-three-next/src/templates/hooks/useSeaLevel.jsx
@@ -2,13 +2,13 @@ import useSWR from 'swr';
 
 const fetcher = (url) => fetch(url).then(res => res.json());
 
-export default function useSeaLevel() {
+export default function useSeaLevel({ scenario = 'ssp245', quantile = '50' }) {
   const { data, error } = useSWR('../../data/sea-level-recife-nested.json', fetcher, {
     revalidateOnFocus: false,
   });
 
   return {
-    data: data?.scenarios?.ssp245?.confidence?.medium?.quantiles?.['50']?.data,
+    data: data?.scenarios?.[scenario]?.confidence?.medium?.quantiles?.[quantile]?.data,
     isLoading: !error && !data,
     error
   };
