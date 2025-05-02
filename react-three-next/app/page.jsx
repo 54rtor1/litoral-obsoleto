0ebc210906598a0b47ce9ae1b6457e1adc2baeb7
54rtor1 <1929sartori@gmail.com>
2025-05-02 16:25:41 -0300

    comments page content

--- react-three-next/app/page.jsx
+++ react-three-next/app/page.jsx
@@ -1,21 +1,14 @@
 'use client'
 
-import { Suspense } from 'react'
-import { View } from '@/components/canvas/View'
-import CoastalParticlesWrapper from '@/components/canvas/CoastalParticlesWrapper'
-import YearCounter from '@/components/dom/YearCounter'
-import ScenarioSelector from '@/components/dom/ScenarioSelector'
-
 export default function Page() {
   return (
     <>
-      <ScenarioSelector />
+      {/* <ScenarioSelector />
       <View>
         <Suspense fallback={null}>
           <CoastalParticlesWrapper />
         </Suspense>
-      </View>
-      <YearCounter />
+      </View> */}
     </>
   )
 }
